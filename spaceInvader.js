180acff644032fd3070f9429ba71eb3128e94240
thomas mpiana <thomas.mpiana@student.ehb.be>
2022-10-05 23:59:12 +0200

    SpaceInvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,41 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+square();
+DrawSpaceInvader();
+
+function square() {
+
+    context.beginPath();
+    context.rect(25, 0, 300, 300);
+    context.fillStyle = "black"
+    context.fill();
+    context.stroke();
+
+}
+
+function DrawSpaceInvader() {
+
+    context.beginPath();
+    context.rect(50, 50, 50, 50);
+    context.rect(100, 50, 50, 50);
+    context.rect(100, 100, 50, 50);
+    context.rect(50, 150, 50, 50);
+    context.rect(150, 150, 50, 50);
+    context.rect(150, 200, 50, 50);
+    context.rect(100, 200, 50, 50);
+    context.rect(200, 200, 50, 50);
+    context.rect(250, 150, 50, 50);
+    context.rect(200, 100, 50, 50);
+    context.rect(200, 50, 50, 50);
+    context.rect(250, 50, 50, 50);
+    context.strokeStyle = "#66FF00"
+    context.fillStyle = "#66FF00"
+    context.fill();
+    context.stroke();
+
+}
